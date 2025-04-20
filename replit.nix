baa9a1dde7e1a9fe4b9a41c4d68221572fac2efa
Omkar Chitale <107796806+ovcGIT@users.noreply.github.com>
2025-04-20 12:13:13 +0530

    Add files via upload

---
+++ replit.nix
@@ -0,0 +1,19 @@
+{pkgs}: {
+  deps = [
+    pkgs.curl
+    pkgs.libGLU
+    pkgs.libGL
+    pkgs.zlib
+    pkgs.tk
+    pkgs.tcl
+    pkgs.openjpeg
+    pkgs.libxcrypt
+    pkgs.libwebp
+    pkgs.libtiff
+    pkgs.libjpeg
+    pkgs.libimagequant
+    pkgs.lcms2
+    pkgs.freetype
+    pkgs.glibcLocales
+  ];
+}
